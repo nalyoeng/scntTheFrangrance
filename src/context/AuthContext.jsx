726482a02093f2534e93eee6f1f9da84306d2564
NalyOeng <oengnaly2005@gmail.com>
2026-04-07 09:37:38 +0700

    final

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,7 +1,6 @@
 import React, { createContext, useState, useEffect, useContext } from 'react';
 import { UserStorage } from '../pages/user';
 
-// 1. Create the Context (The "Cloud")
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
@@ -15,8 +14,8 @@ export const AuthProvider = ({ children }) => {
   const refreshCartCount = () => {
     const cart = JSON.parse(localStorage.getItem("cart") || "[]");
     
-    // We sum up 'quantity_requested'. 
-    // We use || 1 as a fallback just in case an item was added without a qty.
+    // sum up 'quantity_requested'. 
+    // use || 1 as a fallback just in case an item was added without a qty.
     const total = cart.reduce((sum, item) => {
       return sum + (item.quantity_requested || 1);
     }, 0);
@@ -24,13 +23,13 @@ export const AuthProvider = ({ children }) => {
     setCartCount(total);
   };
 
-  // 2. Check localStorage when the app first starts
+  // Check localStorage when the app first starts
  useEffect(() => {
-  // 1. Check if users are already in storage
+  // Check if users are already in storage
   const localUsers = localStorage.getItem('users');
   const localAdmins = localStorage.getItem('admins');
 
-  // 2. If storage is empty, "Seed" it with your JSON data
+  // If storage is empty, "Seed" it with your JSON data
   if (!localUsers) {
     localStorage.setItem('users', JSON.stringify(UserStorage.users));
   }
@@ -38,7 +37,6 @@ export const AuthProvider = ({ children }) => {
     localStorage.setItem('admins', JSON.stringify(UserStorage.adminUsers));
   }
 
-  // 3. Continue with your normal loading logic...
   const savedUser = localStorage.getItem('current_user');
   if (savedUser) setUser(JSON.parse(savedUser));
   
@@ -46,13 +44,13 @@ export const AuthProvider = ({ children }) => {
   setLoading(false);
 }, []);
 
-  // 3. Function to log in
+  // Function to log in
   const login = (userData) => {
     setUser(userData);
     localStorage.setItem('current_user', JSON.stringify(userData));
   };
 
-  // 4. Function to log out
+  // Function to log out
   const logout = () => {
     setUser(null);
     localStorage.removeItem('current_user');
@@ -60,13 +58,13 @@ export const AuthProvider = ({ children }) => {
 
   return (
     <AuthContext.Provider value={{ user, login, logout, loading, cartCount, refreshCartCount }}>
-      {/* We don't render the app until we've finished checking localStorage */}
+      {/* don't render the app until finished checking localStorage */}
       {!loading && children}
     </AuthContext.Provider>
   );
 };
 
-// 5. Create a custom hook to use this context easily
+// Create a custom hook to use this context easily
 export const useAuth = () => {
   const context = useContext(AuthContext);
   if (!context) {
